93cc44a5cf2c5339ef5a80193ef890141d491d12
Isco del Collado <fedelcollado@gmail.com>
2026-05-20 22:16:59 -0600

    feat(*): add props for reactivity [ISCO-260520]

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import { CreateTodoButton } from './CreateTodoButton';
+import React from 'react';
 import { TodoCounter } from './TodoCounter';
 import { TodoItem } from './TodoItem';
 import { TodoList } from './TodoList';
 import { TodoSearch } from './TodoSearch';
 import './App.css';
 
+const defaultTodos = [
+  { text: 'Cortar cebolla', completed: true },
+  { text: 'Tomar el curso de intro a React', completed: false },
+  { text: 'Llorar con la llorona', completed: false },
+  { text: 'LALALALALA', completed: false },
+];
+
 function App() {
   return (
-    <div className="App">
-      <TodoCounter />
+    <React.Fragment>
+      <TodoCounter completed={16} total={25} />
       <TodoSearch />
       <TodoList>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {defaultTodos.map(todo => (
+          <TodoItem 
+            key={todo.text} 
+            text={todo.text} 
+            completed={todo.completed}
+          />
+        ))}
       </TodoList>
       <CreateTodoButton />
-    </div>
+    </React.Fragment>
   );
 }
 
