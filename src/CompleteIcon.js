9c39485f6f6f765c1ddaf7dda7d0e300c86ae5a4
Isco del Collado <fedelcollado@gmail.com>
2026-05-21 18:05:33 -0600

    feat(*): add icons [ISCO-260521]

---
+++ src/CompleteIcon.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { TodoIcon } from './TodoIcon';
+
+
+function CompleteIcon() {
+    return (
+        <TodoIcon
+            type="check"
+            color="gray"
+        />
+    )
+}
+
+export { CompleteIcon };
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,16 +1,20 @@
+import { CompleteIcon } from './CompleteIcon';
+import { DeleteIcon } from './DeleteIcon';
 import './TodoItem.css'
 
 function TodoItem(props) {
   return (
     <li className="TodoItem">
-      <span className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}
+      <CompleteIcon />
+      {/* <span className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}
       onClick={props.onComplete}
-      >V</span>
+      >V</span> */}
       <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>{props.text}</p>
-      <span 
+      <DeleteIcon />
+      {/* <span 
       className={`Icon Icon-delete`}
       onClick={props.onDelete}
-      >X</span>
+      >X</span> */}
     </li>
   );
 }
